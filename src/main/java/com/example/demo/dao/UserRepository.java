e2e5013776baa1981838a7a83e63e4679f511d0a
Tunglam4902 <130317110+Tunglam4902@users.noreply.github.com>
2024-01-04 10:09:17 +0700

    Update UserRepository.java

--- src/main/java/com/example/demo/dao/UserRepository.java
+++ src/main/java/com/example/demo/dao/UserRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserRepository extends JpaRepository<User, Integer> {
     User findByUsernameAndPassword(String username, String password);
     User findByUsername(String username);
+    User findByToken(String token);
 }
