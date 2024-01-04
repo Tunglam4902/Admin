12b9b3806e5e36763904a48a032cfd24c5915ebf
Tunglam4902 <130317110+Tunglam4902@users.noreply.github.com>
2024-01-04 10:03:13 +0700

    Update UserService.java

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -9,4 +9,5 @@ public interface UserService {
     User save(User user);
     User checkLogin(String username, String pasword);
     User findById(int id);
+    User checkUsername(String username);
 }
